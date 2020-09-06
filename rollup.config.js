65582b96c2300b9f3cd4324a732369120f234e2a
harapeko <gmail@harapeko.dev>
2020-09-07 05:20:01 +0900

    add serve & livereload

--- rollup.config.js
+++ rollup.config.js
@@ -1,31 +1,39 @@
+import {
+  terser
+} from "rollup-plugin-terser";
 import html2 from "rollup-plugin-html2";
+import livereload from "rollup-plugin-livereload";
 import resolve from "@rollup/plugin-node-resolve";
+import serve from "rollup-plugin-serve";
 import svelte from "rollup-plugin-svelte";
 import sveltePreprocessor from "svelte-preprocess";
-import { terser } from "rollup-plugin-terser";
 import typescript from "@rollup/plugin-typescript";
 
 const isProduction = process.env.NODE_ENV === "production";
-const dist = isProduction ? "prod" : "dev";
-const minify = isProduction
-  ? {
-      removeComments: true,
-      collapseWhitespace: true,
-      // keepClosingSlash: true,
-    }
-  : false;
+const dist = isProduction ? "./prod" : "./dev";
+const minify = isProduction ? {
+    removeComments: true,
+    collapseWhitespace: true,
+    // keepClosingSlash: true,
+  } :
+  false;
 
 export default {
   input: "./src/index.ts",
   output: {
     name: "index",
-    file: `./${dist}/index.js`,
+    file: `${dist}/index.js`,
     format: "iife",
     sourcemap: !isProduction,
   },
   plugins: [
-    typescript({ sourceMap: !isProduction }),
-    resolve({ browser: true, dedupe: ["svelte"] }),
+    typescript({
+      sourceMap: !isProduction
+    }),
+    resolve({
+      browser: true,
+      dedupe: ["svelte"],
+    }),
     svelte({
       dev: !isProduction,
       preprocess: sveltePreprocessor({
@@ -40,6 +48,8 @@ export default {
       minify,
     }),
     isProduction && terser(),
+    !isProduction && serve(dist),
+    !isProduction && livereload(dist),
   ],
   watch: {
     clearScreen: false,
